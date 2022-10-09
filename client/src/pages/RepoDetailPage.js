12151d0430bec3d35220caa4a37d603f19fc66b2
gingercook <vahitaras@gmail.com>
2022-10-09 17:21:56 -0400

    RepoDetailPage is redesigned, I need to work on a way to get MD data to be converted into HTML

--- client/src/pages/RepoDetailPage.js
+++ client/src/pages/RepoDetailPage.js
@@ -1,19 +1,33 @@
 import { useParams, Link } from 'react-router-dom';
 
+import useFetch from '../hooks/use-fetch';
+
 import classes from './RepoDetailPage.module.css';
 
-const TestPage = () => {
+const RepoDetailPage = (props) => {
     const params = useParams();
+    const allData = useFetch();
+    const { repoId } = params;
 
-    return (
-        <>
-            <Link to='/' className={classes['all-repos']}>
-                <p>{`<= Go Back`}</p>
-            </Link>
-            <div>Repo Detail Page</div>
-            <div>{params.repoId}</div>
-        </>
-    );
+    // Filters to match the correct page number with the repo id
+    const filteredData = allData.filter(data => data.id === +repoId);
+    const pageData = filteredData[0];
+
+    const dataDisplay = allData.length === 0 ?
+        (<p>Loading...</p>) :
+        (
+            <section className={classes['repo-detail']}>
+                <Link to='/'>
+                    <p>{`<= Go Back`}</p>
+                </Link>
+                <p><b>Link:</b> {`https://raw.githubusercontent.com/${pageData.full_name}/master/README.md`}</p>
+                <p><b>Message:</b> I could not find this section!</p>
+                <p><b>Author ID:</b> {pageData.owner.id}</p>
+                <p><b>Recent Commit Date:</b> {new Date(pageData.pushed_at).toLocaleDateString('EN-CA')}</p>
+            </section>
+        );
+
+    return dataDisplay;
 };
 
-export default TestPage;
+export default RepoDetailPage;
